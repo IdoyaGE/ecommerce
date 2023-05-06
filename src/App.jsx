2e65573d1ad81127df1bad4afb42edfc60661078
IdoyaGE <igearkabaia@gmail.com>
2023-05-06 21:22:25 +0200

    commit data productos, css, rutas y links

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"; //instalamos librería react-router-dom para navegar entre distintos componentes de un proyecto
+//Añadimos todas las rutas: Home (están todos los productos), ProductScree (están las fichas individuales de los productos)
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to='/'>Amaiie</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path='/product/:slug' element={<ProductScreen />} />
+            <Route path='/' element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
